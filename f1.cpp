fee1096d90a4ee81413f0768c8322099dd028f06
mamyrbekova <122817142+mamyrbekova@users.noreply.github.com>
2023-09-28 17:24:08 +0600

    Add files via upload

---
+++ f1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+ 
+bool isPrime(int n){
+    if(n < 2) return false;
+
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool nthPrime(int num) {
+    if (num <= 0) return -1;
+    int cnt = 0;
+    int prCnt = 2;
+
+    while (true) {
+        if(isPrime(prCnt)) {
+            cnt++;
+            if(cnt == num) {
+                return prCnt;
+            }
+        }
+        prCnt++;
+           
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int res = nthPrime(n);
+    if(res != -1) {
+        cout << res << endl;
+    }
+    return 0;
+}
